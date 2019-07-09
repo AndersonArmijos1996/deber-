3f2e8f50f1bfcae3c931010accac907ebe2f34e7
Anderson Armijos <52385826+AndersonArmijos1996@users.noreply.github.com>
2019-07-09 14:39:26 -0500

    Instituto Superior Tecnológico Rumiñahui
    Anderson Armijos

---
+++ Fecha.java
@@ -0,0 +1,13 @@
+public class Fecha {
+
+    private int anio;
+
+    public Fecha( int anio) {
+      
+        this.anio = anio;
+    }
+ 
+    public int getAnio() {
+        return anio;
+    }
+}
